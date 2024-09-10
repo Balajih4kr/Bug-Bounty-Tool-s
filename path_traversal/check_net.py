d6840b10afd0850126cd5e9712e530b02492b101
BALAJI A <152677288+Balajih4kr@users.noreply.github.com>
2024-09-10 11:31:03 +0530

    Update check_net.py

--- path_traversal/check_net.py
+++ path_traversal/check_net.py
@@ -1,9 +1,22 @@
 import requests
+import subprocess
 def net(d_name):
+    #subprocess.call(['sudo','apt','install','figlet'])
+    subprocess.call(['figlet','etc / passwd'])
+    print("\n")
+    print("                                                     => BALAJI A    \n")
+    print("checking Connection [!]......")
+
     try:
         response =  requests.get(d_name,timeout=5)
+
+        print("\n")
+
+        print("\n")
         print("[+] Connection Done !!!\n\n")
         return True
     except(requests.ConnectionError,requests.Timeout):
         return False
+
+
 
